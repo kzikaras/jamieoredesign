36584d4917f28c5cb2d471495671e77b14627f12
Kurt Zikaras <kurtzikaras@gmail.com>
2019-05-24 18:31:17 -0400

    Added content and widened detail boxes

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ button.addEventListener('click', (e) => {
     e.preventDefault();
     window.scrollTo({ top: 670 });
 });
+
+// If we are on mobile, remove some showcase content
+setInterval(() => {
+    if (window.innerWidth > 720) {
+        document.getElementById('optional').innerHTML = '<p>We promise to save our customers money and give them reliable craftsmanship</p>';
+    } else {
+        document.getElementById('optional').innerHTML = '';
+    }
+}, 300);
